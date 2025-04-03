b56f4a1d79f4aa20a578434f9804bbd5ac0a9a8a
Sunil <doddi@LAPTOP-28ES9PBG>
2025-04-03 17:13:12 +0530

    Initial commit

--- script.js
+++ script.js
@@ -66,4 +66,8 @@ document.querySelectorAll('.skill-tags span').forEach(tag => {
     tag.addEventListener('mouseout', () => {
         tag.style.transform = 'scale(1)';
     });
-}); 
+});
+
+<rewritten_file>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+</rewritten_file>
